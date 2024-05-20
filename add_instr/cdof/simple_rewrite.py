6327c2694bacc2ba3c6b2339f3876bdf784fe13b
Joseph Wilson <josephcwilson47@gmail.com>
2024-05-20 16:36:35 -0500

    Added notes.md

--- add_instr/cdof/simple_rewrite.py
+++ add_instr/cdof/simple_rewrite.py
@@ -1,5 +1,14 @@
 import gtirb_rewriting.driver
-from gtirb_rewriting import *
+from gtirb_rewriting import (
+    AllFunctionsScope,
+    BlockPosition,
+    FunctionPosition,
+    Pass,
+    Patch,
+    patch_constraints,
+    RewritingContext,
+)
+from gtirb_functions import Function
 
 
 class NopPass(Pass):
@@ -7,7 +16,7 @@ class NopPass(Pass):
     Inserts a nop at the start of every function.
     """
 
-    def begin_module(self, module, functions, context):
+    def begin_module(self, module, functions: list[Function], context: RewritingContext):
         context.register_insert(
             AllFunctionsScope(FunctionPosition.ENTRY, BlockPosition.ENTRY),
             Patch.from_function(self.nop_patch),
@@ -16,6 +25,22 @@ class NopPass(Pass):
     @patch_constraints()
     def nop_patch(self, context):
         return "nop"
+
+
+class SimplePrefetchPass(Pass):
+    """
+    Inserts a prefetch at the start of every function
+    """
+
+    def begin_module(self, module, functions, context):
+        context.register_insert(
+            AllFunctionsScope(FunctionPosition.ENTRY, BlockPosition.ENTRY),
+            Patch.from_function(self.prefetch_patch),
+        )
+
+    @patch_constraints()
+    def prefetch_patch(self, context):
+        return "prefetcht0 (%rax)"
 
 
 if __name__ == "__main__":
